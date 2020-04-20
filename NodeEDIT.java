9dc284fe583ae093e96b36d301c98b8bb37ee511
smn29 <61432360+smn29@users.noreply.github.com>
2020-04-20 15:24:39 -0400

    Create NodeEDIT.java

---
+++ NodeEDIT.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Node{
+	//node method
+	static class Node {
+		int data;
+		int height;
+		Node right;
+		Node left;
+	}
+	
+	//making a new node method
+	static class new_node(int val){
+		int newnode = new Node();
+		
+		newnode.data = val;
+		newnode.right = null;
+		newnode.left = null;
+		newnode.height = 0;
+		
+		return newnode
+	}
+}
