736ed3369089c194ab0b0b59ab4eb55bf692159b
smn29 <61432360+smn29@users.noreply.github.com>
2020-04-20 15:24:05 -0400

    Update iterativeAVLtreeEDIT.java

--- iterativeAVLtreeEDIT.java
+++ iterativeAVLtreeEDIT.java
@@ -1,29 +1,6 @@
 import java.util.*;
 //consulted GeekforGeeks as references
 
-class Node{
-	//node method
-	static class Node {
-		int data;
-		int height;
-		Node right;
-		Node left;
-	}
-	
-	//making a new node method
-	static class new_node(int val){
-		int newnode = new Node();
-		
-		newnode.data = val;
-		newnode.right = null;
-		newnode.left = null;
-		newnode.height = 0;
-		
-		return newnode
-	}
-}
-
-
 class iterativeAVLTree{
 	int bal;
 	Node root;
